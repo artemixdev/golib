6366e1974dd14c66657fc3201fcd229612415fca
Artem Kozlov <artemix9@gmail.com>
2022-08-19 19:37:29 +0400

    fix: slice output in map to

--- collection/map.go
+++ collection/map.go
@@ -2,12 +2,13 @@ package collection
 
 func Map[T, U any](input []T, callback func(index int, element T) U) []U {
 	output := make([]U, len(input))
-	MapTo[T, U](input, output, callback)
+	MapTo[T, U](input, &output, callback)
 	return output
 }
 
-func MapTo[T, U any](input []T, output []U, callback func(index int, element T) U) {
+func MapTo[T, U any](input []T, output *[]U, callback func(index int, element T) U) {
 	for index, element := range input {
-		output[index] = callback(index, element)
+		(*output)[index] = callback(index, element)
 	}
+	*output = (*output)[:len(input)]
 }
--- collection/reduce.go
+++ collection/reduce.go
@@ -1,6 +1,6 @@
 package collection
 
-func Reduce[T, U any](input []T, initial U, callback func(accumulator U, element T) U) U {
+func Reduce[T, U any](initial U, input []T, callback func(accumulator U, element T) U) U {
 	accumulator := initial
 	for _, element := range input {
 		accumulator = callback(accumulator, element)
