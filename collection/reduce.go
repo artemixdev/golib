796d10cbdc6436d9dfbfa19a9b9d05b49040358e
Artem Kozlov <artemix9@gmail.com>
2022-08-19 19:59:46 +0400

    feature: reduce index

--- collection/reduce.go
+++ collection/reduce.go
@@ -1,9 +1,9 @@
 package collection
 
-func Reduce[T, U any](initial U, input []T, callback func(accumulator U, element T) U) U {
-	accumulator := initial
-	for _, element := range input {
-		accumulator = callback(accumulator, element)
+func Reduce[T, U any](initial U, input []T, callback func(acc U, index int, element T) U) U {
+	acc := initial
+	for index, element := range input {
+		acc = callback(acc, index, element)
 	}
-	return accumulator
+	return acc
 }
